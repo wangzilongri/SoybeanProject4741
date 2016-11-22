303aa2503b37a821d1bcded44e536c7a0b37271f
Wang, Zilong <zw243@cornell.edu>
2016-11-22 18:21:11 -0500

    ADABOOST done

--- SoybeanProject4741/Clara_Exp/naive_bayes.R
+++ SoybeanProject4741/Clara_Exp/naive_bayes.R
@@ -1,53 +1,107 @@
 library(mlbench)
 library(e1071)
 library(rpart)
+library(caret)
+
 require(foreign)
 require(nnet)
 require(ggplot2)
 require(reshape2)
 library(ggplot2)
+
+
 install.packages('ggplot2', dep = TRUE)
 
 setwd("C:/Users/User/Dropbox/SoybeanProject4741/Clara_Exp")
+myvars <- c("YEAR", "CHECK", "RM", "YIELD", "FAM_SEGMENT1", "FAM_SEGMENT2", "FAM_SEGMENT3", "FAM_SEGMENT4", "FAM_SEGMENT5", "CLASS_OF", "GRAD")
+
 train1 <- read.csv("../Split_Data/training[1].csv")
-
-# select variables v1, v2, v3
-
-myvars <- c("CHECK", "RM_GRP", "YIELD", "CLASS_OF", "GRAD")
 train1 <- train1[myvars]
-
-#train1$YEAR <- as.factor(train1$YEAR)
-#train1$EXPERIMENT <- as.factor(train1$EXPERIMENT)
-#train1$RM <- as.factor(train1$RM)
-
-#train1$LOCATION <- as.factor(train1$LOCATION)
-train1$RM_GRP <- as.factor(train1$RM_GRP)
-train1$YIELD_GRP <- as.factor(train1$YIELD_GRP)
-
+train1$YEAR <- as.factor(train1$YEAR)
 train1$GRAD <- as.factor(train1$GRAD)
 train1$CLASS_OF <- as.factor(train1$CLASS_OF)
 
+train2 <- read.csv("../Split_Data/training[2].csv")
+train2 <- train2[myvars]
+train2$YEAR <- as.factor(train2$YEAR)
+train2$GRAD <- as.factor(train2$GRAD)
+train2$CLASS_OF <- as.factor(train2$CLASS_OF)
 
+train3 <- read.csv("../Split_Data/training[3].csv")
+train3 <- train3[myvars]
+train3$YEAR <- as.factor(train3$YEAR)
+train3$GRAD <- as.factor(train3$GRAD)
+train3$CLASS_OF <- as.factor(train3$CLASS_OF)
+
+train4 <- read.csv("../Split_Data/training[4].csv")
+train4 <- train4[myvars]
+train4$YEAR <- as.factor(train4$YEAR)
+train4$GRAD <- as.factor(train4$GRAD)
+train4$CLASS_OF <- as.factor(train4$CLASS_OF)
+
+train5 <- read.csv("../Split_Data/training[5].csv")
+train5 <- train5[myvars]
+train5$YEAR <- as.factor(train5$YEAR)
+train5$GRAD <- as.factor(train5$GRAD)
+train5$CLASS_OF <- as.factor(train5$CLASS_OF)
+
+
+
+
+# test sets # 
 test1 <-read.csv("../Split_Data/test_2010.csv")
 test1 <- test1[myvars]
-test1 <- test1[,-1]
-
-#test1$LOCATION <- as.factor(test1$LOCATION)
-train1$RM_GRP <- as.factor(train1$RM_GRP)
+test1$YEAR <- as.factor(test1$YEAR)
 test1$CLASS_OF <- as.factor(test1$CLASS_OF)
 test1$GRAD <- as.factor(test1$GRAD)
 
 
+test2 <-read.csv("../Split_Data/test_2011.csv")
+test2 <- test2[myvars]
+test2$YEAR <- as.factor(test2$YEAR)
+test2$CLASS_OF <- as.factor(test2$CLASS_OF)
+test2$GRAD <- as.factor(test2$GRAD)
 
 
-train1$GRAD <- as.numeric(train1$GRAD)
-train1$GRAD <- as.factor(train1$GRAD)
-ggplot(train1, aes(train1$RM_GRP, train1$YIELD, color = train1$FAMILY))+geom_point()
+
+test3 <-read.csv("../Split_Data/test_2012.csv")
+test3 <- test3[myvars]
+test3$YEAR <- as.factor(test3$YEAR)
+test3$CLASS_OF <- as.factor(test3$CLASS_OF)
+test3$GRAD <- as.factor(test3$GRAD)
 
 
-str(train1)
-ggplot(train1, aes(train1$RM_GRP, train1$YIELD, color = train1$EXPRIMENT))+geom_point()
 
+test4 <-read.csv("../Split_Data/test_2013.csv")
+test4 <- test4[myvars]
+test4$YEAR <- as.factor(test4$YEAR)
+test4$CLASS_OF <- as.factor(test4$CLASS_OF)
+test4$GRAD <- as.factor(test4$GRAD)
+
+
+
+test5 <-read.csv("../Split_Data/test_2014.csv")
+test5 <- test5[myvars]
+test5$YEAR <- as.factor(test5$YEAR)
+test5$CLASS_OF <- as.factor(test5$CLASS_OF)
+test5$GRAD <- as.factor(test5$GRAD)
+
+
+library(lattice)
+barchart(test5$~Reason,data=Reasonstats,groups=Catergory, 
+         scales=list(x=list(rot=90,cex=0.8)))
+
+
+ggplot(test5, aes(test5$GRAD, Species, fill = Category)) + 
+  geom_bar(stat="identity", position = "dodge") + 
+  scale_fill_brewer(palette = "Set1")
+
+
+
+counts <- table(mtcars$vs, mtcars$gear)
+barplot(counts, main="Car Distribution by Gears and VS",
+        xlab="Number of Gears", col=c("darkblue","red"),
+        legend = rownames(counts), beside=TRUE)
 
 
 
@@ -55,19 +109,54 @@ ggplot(train1, aes(train1$RM_GRP, train1$YIELD, color = train1$EXPRIMENT))+geom_
 
 
 nb_model <- naiveBayes(train1$GRAD~., data = train1)
-
-
-
 pred <- predict(nb_model, test1)
-
 new <- cbind (test1, pred)
-write.csv(new, file = "naivebayes.csv")
+write.csv(new, file = "naivebayes1.csv")
+confusion1 <- confusionMatrix(pred, reference = test1$GRAD, positive='yes')
+
+nb_model <- naiveBayes(train2$GRAD~., data = train2)
+pred <- predict(nb_model, test2)
+new <- cbind (test2, pred)
+write.csv(new, file = "naivebayes2.csv")
+confusion2 <- confusionMatrix(pred, reference = test2$GRAD, positive='yes')
+
+nb_model <- naiveBayes(train3$GRAD~., data = train3)
+pred <- predict(nb_model, test3)
+new <- cbind (test3, pred)
+write.csv(new, file = "naivebayes3.csv")
+confusion3 <- confusionMatrix(pred, reference = test3$GRAD, positive='yes')
 
 
-confusion <- table (pred, test1$GRAD)
+nb_model <- naiveBayes(train4$GRAD~., data = train4)
+pred <- predict(nb_model, test4)
+new <- cbind (test4, pred)
+write.csv(new, file = "naivebayes4.csv")
+confusion4 <- confusionMatrix(pred, reference = test4$GRAD, positive='yes')
 
-########################################
 
+nb_model <- naiveBayes(train5$GRAD~., data = train5)
+pred <- predict(nb_model, test5)
+new <- cbind (test5, pred)
+write.csv(new, file = "naivebayes5.csv")
+confusion5 <-confusionMatrix(pred, reference = test5$GRAD, positive='yes')
+
+
+confusion1
+confusion2
+confusion3
+confusion4
+confusion5
+
+
+precision <- confusion1$byClass['Pos Pred Value']    
+recall <- confusion1$byClass['Sensitivity']
+f_measure1 <- 2 * ((precision * recall) / (precision + recall))
+
+f_measure1
+
+
+
+#K MEANS# 
 
 
 set.seed(20)
--- SoybeanProject4741/Zilong_Exp/Adaboost_training[1].R
+++ SoybeanProject4741/Zilong_Exp/Adaboost_training[1].R
@@ -7,19 +7,55 @@
 #require(nnet);
 #require(ggplot2);
 #require(reshape2);
-
+#install.packages("fastAdaboost");
+library(nnet);
+library(e1071);
+library(caret);
+library(fastAdaboost);
 library(rpart);
-library(adabag);
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 
-training = read.table("../Split_Data/training[1].csv",sep=",",header=TRUE);
-testing = read.table("../Split_Data/test_2010.csv",sep=",",header=TRUE);
+training_ = read.table("../Split_Data/training[1].csv",sep=",",header=TRUE);
+testing_ = read.table("../Split_Data/test_2010.csv",sep=",",header=TRUE);
 
-training$GRAD <- as.factor(training$GRAD);
-testing$GRAD <- as.factor(testing$GRAD);
 
-training.adaboost <- boosting( GRAD ~.,data=training,boos=TRUE,mfinal=2);
-importanceplot(training.adaboost);
-save.image(file = "training_done.RData");
+#Cut out YEAR, EXPERIMENT, LOCATION, VARIETY, FAMILY, RM, YIELD
+
+#Use 
+#X: CHECK, RM, YIELD, Family segments, CLASS_OF
+#y: GRAD as a factor
+
+training <- subset( training_,select=c("YEAR","CHECK","RM", "YIELD","FAM_SEGMENT1","FAM_SEGMENT2","FAM_SEGMENT3","FAM_SEGMENT4","FAM_SEGMENT5","CLASS_OF", "GRAD" ) );
+testing <- subset( testing_,select=c("YEAR","CHECK","RM", "YIELD","FAM_SEGMENT1","FAM_SEGMENT2","FAM_SEGMENT3","FAM_SEGMENT4","FAM_SEGMENT5","CLASS_OF", "GRAD" ));
+
+training = training[training$GRAD != -1,];
+training$GRAD <- factor(training$GRAD); 
+
+testing = testing[testing$GRAD != -1,];
+testing$GRAD <- factor(testing$GRAD); 
+
+test_adaboost <- adaboost(GRAD~.,data=training,10);
+pred <- predict(test_adaboost,newdata = testing);
+
+#Test error
+#print( paste("error is", toString(pred$error) )  );
+
+# Confusion matrix
+#print( table(pred$class,testing$GRAD) );
+
+confusion1 <- confusionMatrix(pred$class,reference = testing$GRAD);
+
+# Length of outcomes
+l = length(pred$class);
+
+# Number correct
+correctNum = sum(pred$class == testing$GRAD);
+
+# Percentage correct
+pCorrect = correctNum/l;
+
+#print( paste("Proportion correct is",toString(pCorrect))    );
+
+print(confusion1)
